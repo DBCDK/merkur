dcdb65e2376dd5bc01a5eebce8d87ee22f5997ac
Jan Bauer Nielsen <jbn@dbc.dk>
2018-04-25 09:49:05 +0200

    Move file related methods to FilesController

--- src/controllers/FilesController.js
+++ src/controllers/FilesController.js
@@ -5,8 +5,10 @@
 
 import url from "url";
 import constants from "../constants";
+import Busboy from "busboy";
 import StoresConnector from "../StoresConnector";
 import AgencyIdConverter from "../model/AgencyIdConverter";
+import FileMetadata from "../model/FileMetadata";
 import * as AuthController from "./AuthController";
 
 const mapToUtc = (millisecondsSinceEpoch) => {
@@ -37,6 +39,16 @@ const mapToFileUrl = (request, fileAttributes, path) => {
         host: request.get('host'),
         pathname: path.replace(':id', fileAttributes.id)
     });
+};
+
+const getFile = (req, res) => {
+    const agency = AuthController.authenticate(req, res);
+    if (agency !== undefined) {
+        StoresConnector.getFile(req.params.id).end().then(response => {
+            res.status(200).send(response.body)
+        }).catch(err => res.status(500).send(
+            `error while getting file ${req.params.id}: ${err}`));
+    }
 };
 
 const getFiles = (req, res) => {
@@ -80,4 +92,120 @@ const postFileClaimed = (req, res) => {
     }
 };
 
-export {getFiles, getUnclaimedFiles, postFileClaimed}
+const searchFiles = (req, res) => {
+    // Due to the current design of the main page in the UI
+    // this method can not be authenticated, else a 401
+    // code is returned to the browser
+    StoresConnector.searchFiles(req.body).end().then(json =>
+        res.status(200).send(json.text)
+    ).catch(err => res.status(500).send(err));
+};
+
+const uploadMetadata = (req, res) => {
+    const agency = AuthController.authenticate(req, res);
+    if (agency !== undefined) {
+        const url = req.body.url;
+        const metadata = req.body.metadata;
+        if (url === undefined || url === null) {
+            return res.status(400).send("request missing url in json body");
+        } else if (metadata === undefined || metadata === null) {
+            return res.status(400).send("request missing metadata in json body");
+        } else if (!FileMetadata.verify(metadata)) {
+            return res.status(400).send(`request metadata ` +
+                `${JSON.stringify(metadata)} does not pass validation`);
+        }
+        StoresConnector.addMetadata(url, metadata)
+            .end().then(json =>
+            res.status(200).send(json)
+        ).catch(err => res.status(500).send(err));
+    }
+};
+
+const uploadFile = (req, res) => {
+    const agency = AuthController.authenticate(req, res);
+    if (agency !== undefined) {
+        if (req.is("multipart/form-data")) {
+            return handleFileFormDataUpload(req, res);
+        } else if (req.is("application/octet-stream")) {
+            return handleFileUpload(req, res);
+        } else {
+            res.sendStatus(415);
+        }
+    }
+};
+
+const handleFileFormDataUpload = (req, res) => {
+    const metadata = {};
+    let promise = null;
+    const busboy = new Busboy({headers: req.headers});
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+        metadata.name = filename;
+        promise = addFile(file).catch(err => handleUploadError(filename, err, res));
+    });
+    busboy.on("field", (fieldname, value, fieldnameTruncated,
+            valueTruncated, encoding, mimetype) => {
+        const valueAsInt = Number.parseInt(value);
+        if (!Number.isNaN(valueAsInt)) {
+            metadata[fieldname] = valueAsInt;
+        } else {
+            metadata[fieldname] = value;
+        }
+    });
+    busboy.on("finish", () => {
+        if (promise === null) {
+            return res.status(400).send("must specify file to upload");
+        }
+        if (metadata.origin === undefined || metadata.origin === null) {
+            metadata.origin = constants.defaultOrigin;
+        }
+        promise.then(addFileResponse => {
+                const url = addFileResponse.headers.location;
+                return handleMetadata(url, metadata, res);
+            }).catch(err => handleUploadError(metadata.name, err, res));
+    });
+    req.pipe(busboy);
+};
+
+const handleUploadError = (filename, err, res) => {
+    if (err.status !== undefined && err.msg !== undefined) {
+        return res.status(err.status).send(err.msg);
+    } else {
+        return res.status(500).send(
+            `error uploading ${filename}: ${err}`);
+    }
+};
+
+const handleFileUpload = (req, res) => {
+    addFile(req).then(json => res.status(200).send(json))
+        .catch(err => {
+            if (err.status !== undefined && err.msg !== undefined) {
+                res.status(err.status).send(err.msg);
+            } else {
+                res.status(500).send(err);
+            }
+        });
+};
+
+const addFile = file => {
+    return new Promise((resolve, reject) => {
+        const request = StoresConnector.addFile(null);
+        let size = 0;
+        file.on("data", buffer => {
+            request.write(buffer);
+            size += buffer.length;
+        });
+        file.on("end", () => {
+            if (size === 0) {
+                reject({
+                    status: 400,
+                    msg: "must supply a non-empty request entity"
+                });
+            }
+            request.end().then(json => resolve(json))
+                .catch(err => reject(err));
+        });
+    });
+};
+
+export {getFile, getFiles, getUnclaimedFiles, postFileClaimed, searchFiles,
+    uploadFile, uploadMetadata}
