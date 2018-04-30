e6313303e0052c4f7e4abcf39876c4300234c7c4
Jan Bauer Nielsen <jbn@dbc.dk>
2018-04-30 08:14:51 +0200

    Return 401 response on invalid credentials

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -105,7 +105,8 @@ const authenticate = (request, response) => {
         return parts[0];
     }
 
-    response.status(403).send("Unknown agency ID or apikey");
+    response.status(401).send("Unknown agency ID or apikey");
+    response.setHeader('WWW-Authenticate', 'Basic realm="DBC merkur"');
     return undefined;
 };
 
